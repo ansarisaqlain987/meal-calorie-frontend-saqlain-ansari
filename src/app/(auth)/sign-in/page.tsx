7205e412aa63cca0467326364b832fbae2fd82f8
Saqlain Ansari <ansarisaqlain987@gmail.com>
2025-07-02 13:46:38 +0530

    initial commit

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>SignIn Page</h1>
+    </div>
+  );
+}
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>SignUp Page</h1>
+    </div>
+  );
+}
